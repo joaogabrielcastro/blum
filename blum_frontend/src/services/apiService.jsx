670d727e7531b16625cdc2a98e14e50c44a6ed62
João Wotekoski - E. Escolinha de TI - BNE <joaowotekoski@bne.com.br>
2025-11-12 14:04:29 -0300

    Criação do .env para a rl do render e  localhost

--- blum_frontend/src/services/apiService.jsx
+++ blum_frontend/src/services/apiService.jsx
@@ -1,6 +1,6 @@
 // services/apiService.js
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3000/api/v1";
+const API_URL = process.env.REACT_APP_API_URL;
 
 
 const apiService = {
